Fix genotype calculation bug and set rsid as index

src/snps/io.py
@@ -499,12 +499,13 @@ class Reader:
         df['rsid'] = df['SNP Name'].apply(map_codigo_rsids)
         df['chrom'] = df['SNP Name'].apply(map_codigo_chr)
         df['pos'] = df['SNP Name'].apply(map_codigo_pos)
-        df['genotype'] = df['Allele1 - Plus'] + df['Allele1 - Plus']
+        df['genotype'] = df['Allele1 - Plus'] + df['Allele2 - Plus']
 
         df.dropna(inplace=True)
 
         df = df.astype({"chrom": object, "pos": np.int64})
         df = df[["rsid", "chrom", "pos", "genotype"]]
+        df.set_index(["rsid"], inplace=True)
 
         return df, "Codigo46"
 
72f95f5b14b363669948e43b38e051dbcbd9e142
Andrew Riha <apriha@gmail.com>
2020-05-02 21:36:52 -0700

    Rearrange tests

--- tests/io/test_reader.py
+++ tests/io/test_reader.py
@@ -39,44 +39,6 @@ from tests import BaseSNPsTestCase
 
 
 class TestReader(BaseSNPsTestCase):
-    def test_read_DNALand(self):
-        # https://dna.land/
-        self.run_parsing_tests("tests/input/DNALand.txt", "DNA.Land")
-
-    def test_read_23andme(self):
-        # https://www.23andme.com
-        self.run_parsing_tests("tests/input/23andme.txt", "23andMe")
-
-    def test_read_23andme_zip(self):
-        zip_file(
-            "tests/input/23andme.txt", "tests/input/23andme.txt.zip", "23andme.txt"
-        )
-        self.run_parsing_tests("tests/input/23andme.txt.zip", "23andMe")
-
-    def test_read_ftdna(self):
-        # https://www.familytreedna.com
-        self.run_parsing_tests("tests/input/ftdna.csv", "FTDNA")
-
-    def test_read_ftdna_gzip(self):
-        gzip_file("tests/input/ftdna.csv", "tests/input/ftdna.csv.gz")
-        self.run_parsing_tests("tests/input/ftdna.csv.gz", "FTDNA")
-
-    def test_read_ftdna_famfinder(self):
-        # https://www.familytreedna.com
-        self.run_parsing_tests("tests/input/ftdna_famfinder.csv", "FTDNA")
-
-    def test_read_ancestry(self):
-        # https://www.ancestry.com
-        self.run_parsing_tests("tests/input/ancestry.txt", "AncestryDNA")
-
-    def test_read_genes_for_good(self):
-        # https://genesforgood.sph.umich.edu/
-        self.run_parsing_tests("tests/input/genesforgood.txt", "GenesForGood")
-
-    def test_read_myheritage(self):
-        # https://www.myheritage.com
-        self.run_parsing_tests("tests/input/myheritage.csv", "MyHeritage")
-
     @staticmethod
     def _setup_gsa_test():
         # reset resource if already loaded
@@ -95,47 +57,41 @@ class TestReader(BaseSNPsTestCase):
         os.remove("resources/gsa_rsid_map.txt.gz")
         os.remove("resources/gsa_chrpos_map.txt.gz")
 
+    def test_read_23andme(self):
+        # https://www.23andme.com
+        self.run_parsing_tests("tests/input/23andme.txt", "23andMe")
+
+    def test_read_23andme_zip(self):
+        zip_file(
+            "tests/input/23andme.txt", "tests/input/23andme.txt.zip", "23andme.txt"
+        )
+        self.run_parsing_tests("tests/input/23andme.txt.zip", "23andMe")
+
+    def test_read_ancestry(self):
+        # https://www.ancestry.com
+        self.run_parsing_tests("tests/input/ancestry.txt", "AncestryDNA")
+
     def test_read_codigo46(self):
         # https://codigo46.com.mx
         self._setup_gsa_test()
         self.run_parsing_tests("tests/input/codigo46.txt", "Codigo46")
         self._teardown_gsa_test()
 
-    def test_read_sano(self):
-        # https://sanogenetics.com
-        self._setup_gsa_test()
-        self.run_parsing_tests("tests/input/sano.txt", "Sano")
-        self._teardown_gsa_test()
+    def test_read_DNALand(self):
+        # https://dna.land/
+        self.run_parsing_tests("tests/input/DNALand.txt", "DNA.Land")
 
-    def test_read_livingdna(self):
-        # https://livingdna.com
-        self.run_parsing_tests("tests/input/livingdna.csv", "LivingDNA")
+    def test_read_ftdna(self):
+        # https://www.familytreedna.com
+        self.run_parsing_tests("tests/input/ftdna.csv", "FTDNA")
 
-    def test_read_mapmygenome(self):
-        # https://mapmygenome.in
-        self.run_parsing_tests("tests/input/mapmygenome.txt", "Mapmygenome")
+    def test_read_ftdna_gzip(self):
+        gzip_file("tests/input/ftdna.csv", "tests/input/ftdna.csv.gz")
+        self.run_parsing_tests("tests/input/ftdna.csv.gz", "FTDNA")
 
-    def test_read_vcf(self):
-        self.run_parsing_tests_vcf("tests/input/testvcf.vcf")
-
-    def test_read_vcf_phased(self):
-        self.run_parsing_tests_vcf("tests/input/testvcf_phased.vcf", phased=True)
-
-    def test_read_vcf_rsids(self):
-        self.run_parsing_tests_vcf("tests/input/testvcf.vcf", rsids=["rs1", "rs2"])
-
-    def test_read_vcf_gz(self):
-        gzip_file("tests/input/testvcf.vcf", "tests/input/testvcf.vcf.gz")
-        self.run_parsing_tests_vcf("tests/input/testvcf.vcf.gz")
-
-    def test_read_unannotated_vcf(self):
-        self.run_parsing_tests_vcf(
-            "tests/input/unannotated_testvcf.vcf", unannotated=True
-        )
-
-    def test_read_not_phased(self):
-        s = SNPs("tests/input/generic.csv")
-        assert not s.phased
+    def test_read_ftdna_famfinder(self):
+        # https://www.familytreedna.com
+        self.run_parsing_tests("tests/input/ftdna_famfinder.csv", "FTDNA")
 
     def test_read_generic_csv(self):
         self.run_parsing_tests("tests/input/generic.csv", "generic")
@@ -153,3 +109,47 @@ class TestReader(BaseSNPsTestCase):
 
     def test_read_generic_extra_column_tsv(self):
         self.run_parsing_tests("tests/input/generic_extra_column.tsv", "generic")
+
+    def test_read_genes_for_good(self):
+        # https://genesforgood.sph.umich.edu/
+        self.run_parsing_tests("tests/input/genesforgood.txt", "GenesForGood")
+
+    def test_read_livingdna(self):
+        # https://livingdna.com
+        self.run_parsing_tests("tests/input/livingdna.csv", "LivingDNA")
+
+    def test_read_mapmygenome(self):
+        # https://mapmygenome.in
+        self.run_parsing_tests("tests/input/mapmygenome.txt", "Mapmygenome")
+
+    def test_read_myheritage(self):
+        # https://www.myheritage.com
+        self.run_parsing_tests("tests/input/myheritage.csv", "MyHeritage")
+
+    def test_read_sano(self):
+        # https://sanogenetics.com
+        self._setup_gsa_test()
+        self.run_parsing_tests("tests/input/sano.txt", "Sano")
+        self._teardown_gsa_test()
+
+    def test_read_vcf(self):
+        self.run_parsing_tests_vcf("tests/input/testvcf.vcf")
+
+    def test_read_vcf_gz(self):
+        gzip_file("tests/input/testvcf.vcf", "tests/input/testvcf.vcf.gz")
+        self.run_parsing_tests_vcf("tests/input/testvcf.vcf.gz")
+
+    def test_read_vcf_phased(self):
+        self.run_parsing_tests_vcf("tests/input/testvcf_phased.vcf", phased=True)
+
+    def test_read_vcf_rsids(self):
+        self.run_parsing_tests_vcf("tests/input/testvcf.vcf", rsids=["rs1", "rs2"])
+
+    def test_read_unannotated_vcf(self):
+        self.run_parsing_tests_vcf(
+            "tests/input/unannotated_testvcf.vcf", unannotated=True
+        )
+
+    def test_read_not_phased(self):
+        s = SNPs("tests/input/generic.csv")
+        assert not s.phased
